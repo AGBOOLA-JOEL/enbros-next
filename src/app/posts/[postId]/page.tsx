ef7f7032aa5a95eb7f26b8d764469b8ce3cfb829
joel oluwafisayo <122305817+AGBOOLA-JOEL@users.noreply.github.com>
2025-06-29 23:08:53 +0100

    fix

--- src/app/posts/[postId]/page.tsx
+++ src/app/posts/[postId]/page.tsx
@@ -1,3 +1,47 @@
+// import BlogViewpage from "@/components/general/blog-viewpage";
+// import { BlogPost } from "@/types/posts.type";
+// import { notFound } from "next/navigation";
+
+// export const revalidate = false;
+
+// type PageProps = {
+//   params: {
+//     postId: string;
+//   };
+// };
+
+// async function getPost(postId: string): Promise<BlogPost | null> {
+//   const res = await fetch(
+//     `${process.env.NEXT_PUBLIC_API_BACKEND_URL}/posts/${postId}`,
+//     {
+//       headers: { "Content-Type": "application/json" },
+//       next: { revalidate: 0 },
+//     }
+//   );
+
+//   if (!res.ok) {
+//     console.error(`Failed to fetch post ${postId}`);
+//     return null;
+//   }
+
+//   return res.json();
+// }
+
+// export default async function PostPage({ params }: PageProps) {
+//   const post = await getPost(params.postId);
+
+//   if (!post) {
+//     notFound();
+//   }
+
+//   return (
+//     <div className="blog__view">
+//       <div className="blog__view__display">
+//         <BlogViewpage data={post} />
+//       </div>
+//     </div>
+//   );
+// }
 import BlogViewpage from "@/components/general/blog-viewpage";
 import { BlogPost } from "@/types/posts.type";
 import { notFound } from "next/navigation";
@@ -5,9 +49,7 @@ import { notFound } from "next/navigation";
 export const revalidate = false;
 
 type PageProps = {
-  params: {
-    postId: string;
-  };
+  params: { postId: string };
 };
 
 async function getPost(postId: string): Promise<BlogPost | null> {
