85fcb90a2ed688e14b46f07e5e5a3755d02d0b77
joel oluwafisayo <122305817+AGBOOLA-JOEL@users.noreply.github.com>
2025-06-29 23:13:38 +0100

    post page fix

--- src/app/posts/[postId]/page.tsx
+++ src/app/posts/[postId]/page.tsx
@@ -48,10 +48,6 @@ import { notFound } from "next/navigation";
 
 export const revalidate = false;
 
-type PageProps = {
-  params: { postId: string };
-};
-
 async function getPost(postId: string): Promise<BlogPost | null> {
   const res = await fetch(
     `${process.env.NEXT_PUBLIC_API_BACKEND_URL}/posts/${postId}`,
@@ -69,7 +65,11 @@ async function getPost(postId: string): Promise<BlogPost | null> {
   return res.json();
 }
 
-export default async function PostPage({ params }: PageProps) {
+export default async function PostPage({
+  params,
+}: {
+  params: { postId: string };
+}) {
   const post = await getPost(params.postId);
 
   if (!post) {
